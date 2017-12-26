8993f859f5c7e428e86d5537358bb56ed2231352
Rodrigo Soares <oncemade@gmail.com>
2017-12-25 16:24:30 -0800

    Find and replace Title

--- src/commandFindReplace.js
+++ src/commandFindReplace.js
@@ -4,27 +4,26 @@
  * @Project: Rename It
  * @Last modified time: 2017-12-02T11:44:14-08:00
  */
-import { parseData } from './lib/utils';
-import theUI from './lib/TheUI'
+import { parseData } from "./lib/utils"
+import theUI from "./lib/TheUI"
 
 export default function(context) {
-  const data = parseData(context);
+  const data = parseData(context)
 
   // Return if there is no selection and show message
   if (data.selectionCount <= 0) {
-    context.document.showMessage("Rename it: You need to select at least one layer or artboard");
-    return;
+    context.document.showMessage("Rename it: You need to select at least one layer or artboard")
+    return
   }
 
   const options = {
     identifier: "findReplace.ui",
-    title: "Rename Selected Layers",
+    title: "Find & Replace Layers",
     redirectTo: "/find_replace",
     width: 480,
-    height: 300
+    height: 300,
   }
 
   // Load UI
-  theUI(context, data, options);
-
+  theUI(context, data, options)
 }
